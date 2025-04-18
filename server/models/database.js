b8e8adcf7d0d59db1a780ab086083d8f2bd0d244
vasudevshetty <vasudeepu2815@gmail.com>
2025-04-18 23:28:09 +0530

    fixed so many things, and got a way to start with the flow of the project.

---
+++ server/models/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const databaseSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    host: { type: String, required: true },
+    userName: { type: String, required: true }, // DB user
+    password: { type: String, required: true }, // Consider encrypting
+    database: { type: String, required: true },
+    dbType: { type: String, enum: ["mysql", "postgresql"], required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Database", databaseSchema);
