6b9cee8ce153a2a4ca87b50bfc1a5f86eda6d804
aniruddhapai27 <aniruddhaanantpai@gmail.com>
2025-04-18 20:26:48 +0530

    Updated the layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Graph from "./components/Graph";
 import Home from "./pages/Home";
+import Layout from "./components/Layout";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/visual" element={<Graph />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/visual" element={<Graph />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
     </Routes>
   );
 }
